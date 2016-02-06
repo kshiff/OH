0a907be3ebbc34949958a358c4290274fd437018
Karl Shiffler <karlshiffler@gmail.com>
2016-02-06 18:34:09 -0500

    refactored podcast.py to make it importable

--- podcast.py
+++ podcast.py
@@ -2,60 +2,94 @@
 Karl Shiffler 
 karlshiffler@gmail.com
 
-Script to download podcast information from RSS feed URLs. Currently pulls series title, episode title, description.
+Command line python script to download podcast information from RSS feed URLs. Currently pulls series title, episode title, description.
+
+
 '''
 import feedparser
 import csv
 import re
-import requests
-from bs4 import BeautifulSoup
-import simplejson as json
+import argparse
+from tqdm import tqdm
+import sys
 
-feeds = []
+def intakeFile(filename):
+	'''
+	Read in URLs from list file and return list of URLs 
 
-# Read in from list of URLs 
-with open('RSS-Feeds_Karl.csv', 'rb') as f: 
-	reader = csv.reader(f, delimiter=',')
-	next(reader, None)
-	for row in reader:
-		# print row
-		feeds.append(row[2])
-	
+	Args:
+		filename (str): name of input file if in same directory as script, full/relative path otherwise
+
+	Returns:
+		list: list of URLs
+	'''
+	feeds = []
+	with open(filename, 'rb') as f: 
+		reader = csv.reader(f, delimiter=',')
+		next(reader, None)
+		for row in reader:
+			feeds.append(row[2])
+	return feeds	
 	
 # Write results to file
-with open('summaryData.csv','wb') as out:
-	sw = csv.writer(out, delimiter=',')
-	sw.writerow(['provider', 'episode', 'summary'])
-	for link in feeds:
-		# print link
-		pod = feedparser.parse(link)
-		# print pod.bozo
+def writeData(outfile, feeds):
+	errors = []
+	with open(outfile,'wb') as out:
+		sw = csv.writer(out, delimiter=',')
+		sw.writerow(['provider', 'episode', 'summary'])
+		for link in tqdm(feeds):
+			# print link
+			pod = feedparser.parse(link)
+			# print pod.bozo
+			try:
+				provider = pod.feed.title
 
-		provider = pod.feed.title
-		provider = provider.encode('ascii','ignore')
+				provider = provider.encode('ascii','ignore')
 
-		for i in range(len(pod.entries)):
-			episode = pod.entries[i].title
-			episode = episode.encode('ascii','ignore')
+				for i in range(len(pod.entries)):
+					episode = pod.entries[i].title
+					episode = episode.encode('ascii','ignore')
 
-			# check if summary is truncated
-			data = pod.entries[i].summary
-			# data = pod.entries[0].content[0].value
+					# check if summary is truncated
+					data = pod.entries[i].summary
+					# data = pod.entries[0].content[0].value
 
-			data = data.encode('ascii','ignore')
-			data = re.sub(r'\n', '', data)
-			data = re.sub(r'\<.*?\>', '', data)
-			data = re.sub(r'\&.*?\;', '', data)
-			data = re.sub(r'\{.*?\}', '', data)
-			
+					data = data.encode('ascii','ignore')
+					data = re.sub(r'\n', '', data)
+					data = re.sub(r'\<.*?\>', '', data)
+					data = re.sub(r'\&.*?\;', '', data)
+					data = re.sub(r'\{.*?\}', '', data)
+					
 
-			# if data.endswith("[...]"):
-			# 	print "truncated"
+					# if data.endswith("[...]"):
+					# 	print "truncated"
 
-			
-			print provider
-			print episode
-			print data
-			print "\n----------------------------------------------------------------------\n"
-			sw.writerow([provider,episode,data])
+					
+					#print provider
+					#print episode
+					#print data
+					#print "\n----------------------------------------------------------------------\n"
+					sw.writerow([provider,episode,data])
+
+
+
+			except AttributeError:
+				errors.append([pod.feed, link])
+	print 'Encountered ' + str(len(errors)) + ' errors in this list.'
+
+
+def main(argv):
+	parser = argparse.ArgumentParser(description='Commandline python script that allows reading list of podcast URLs and generating a file containing desciptive data')
+	parser.add_argument('podfile', type=str, help='filename of the podfile')
+	parser.add_argument('--output', '-o', type=str, help='filename the podcast data will be output to')
+
+	args = parser.parse_args()
+
+	feeds = intakeFile(args.podfile)
+	writeData(args.output, feeds)
+
+if __name__ == "__main__":
+	main(sys.argv)
+
+
 
